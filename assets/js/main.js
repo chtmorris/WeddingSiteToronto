05bdd5d2285cf2562fc66bd26584a86409f8efe3
chtmorris <agoosester@gmail.com>
2016-06-12 10:13:03 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//   console.log("yoyo");
+//   $('input[type=checkbox]').change(
+//     function(){
+//         if (this.checked) {
+//             console.log(this.name);
+//             $('#message').val($('#message').val()+"I will be attending the " + this.name + "\n");
+//         }
+//     });
+// });
